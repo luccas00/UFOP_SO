1d8b378d5b95864bb9912bcb536c75c16e4bcf9f
Luccas Carneiro MacBook <luccas.carneiro@hotmail.com>
2025-02-10 19:14:05 -0300

    Updates

---
+++ Threads/MetodosDeBloqueio/Peterson/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!");
+
+        // ThreadExtends t1 = new ThreadExtends("Thread 1");
+        // ThreadExtends t2 = new ThreadExtends("Thread 2");
+        // ThreadExtends t3 = new ThreadExtends("Thread 3");
+
+        // t1.start();
+        // t2.start();
+
+        // try {
+        //     t1.join();
+        //     t2.join();
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        
+    }
+}
